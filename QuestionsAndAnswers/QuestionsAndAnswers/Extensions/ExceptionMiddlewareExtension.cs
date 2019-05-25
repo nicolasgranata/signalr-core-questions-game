cf2e1df2a61c532830835734c1f497613f72d1f4
Nicolas Granata <nicolas.granata@southworks.com>
2019-05-24 22:07:16 -0300

    Add exception middleware extension handler, create project initial structure

---
+++ QuestionsAndAnswers/QuestionsAndAnswers/Extensions/ExceptionMiddlewareExtension.cs
@@ -0,0 +1,34 @@
+﻿using System.Net;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Http;
+using QuestionsAndAnswers.Models;
+
+namespace QuestionsAndAnswers.Extensions
+{
+    public static class ExceptionMiddlewareExtension
+    {
+        public static void ConfigureExceptionHandler(this IApplicationBuilder app)
+        {
+            app.UseExceptionHandler(appError =>
+            {
+                appError.Run(async context =>
+                {
+                    context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+                    context.Response.ContentType = "application/json";
+
+                    var contextFeature = context.Features.Get<IExceptionHandlerFeature>();
+
+                    if (contextFeature != null)
+                    {                 
+                        await context.Response.WriteAsync(new ErrorDetail()
+                        {
+                            StatusCode = context.Response.StatusCode,
+                            Message = "Internal Server Error."
+                        }.ToString());
+                    }
+                });
+            });
+        }
+    }
+}
---
+++ QuestionsAndAnswers/QuestionsAndAnswers/Models/ErrorDetail.cs
@@ -0,0 +1,8 @@
+﻿namespace QuestionsAndAnswers.Models
+{
+    public class ErrorDetail
+    {
+        public int StatusCode { get; set; }
+        public string Message { get; set; }
+    }
+}
