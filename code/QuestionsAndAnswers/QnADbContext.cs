59d5afc06c46741df3fd4431020f85149d1ff70a
Nicolas Granata <nicolas.granata@southworks.com>
2019-06-07 15:51:59 -0300

    Add QuestionAnswers Db Context with in memory database

---
+++ code/QuestionsAndAnswers/QnADbContext.cs
@@ -0,0 +1,38 @@
+﻿using System.IO;
+using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json;
+using QuestionsAndAnswers.Models;
+
+namespace QuestionsAndAnswers
+{
+    public class QnADbContext: DbContext
+    {
+        public QnADbContext(DbContextOptions<QnADbContext> options)
+            : base(options)
+        {
+
+        }
+
+        public DbSet<QuestionAnswers> QuestionAnswers { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            QuestionAnswersData questionAnswersData;
+            using (var stream = File.OpenRead("QnA.json"))
+            using (var streamReader = new StreamReader(stream))
+            using (var reader = new JsonTextReader(streamReader))
+            {
+                var serializer = new JsonSerializer();
+                questionAnswersData = serializer.Deserialize<QuestionAnswersData>(reader);
+            }
+
+            modelBuilder.Entity<QuestionAnswers>()
+                .HasData(questionAnswersData.QuestionAnswers);
+
+            modelBuilder.Entity<QuestionAnswers>()
+            .Ignore(p => p.Options);
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
--- code/QuestionsAndAnswers/Startup.cs
+++ code/QuestionsAndAnswers/Startup.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using QuestionsAndAnswers.Extensions;
 using QuestionsAndAnswers.Hubs;
@@ -17,11 +18,16 @@ namespace QuestionsAndAnswers
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<QnADbContext>(option =>
+            {
+                option.UseInMemoryDatabase("QnaSignalR");
+            });
+
             services.AddSignalR();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, QnADbContext context)
         {
             app.ConfigureExceptionHandler();
 
@@ -34,6 +40,9 @@ namespace QuestionsAndAnswers
             {
                 routes.MapHub<QuestionsAndAnswersHub>("/chatHub");
             });
+
+            // Force database seeding to execute
+            context.Database.EnsureCreated();
         }
     }
 }
